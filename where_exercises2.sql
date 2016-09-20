6dadce2beeba83a2de28d1e59e160a8f05f94ad3
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-20 09:49:05 -0500

    Update: Solved for names ending in E.

--- where_exercises2.sql
+++ where_exercises2.sql
@@ -1,3 +1,10 @@
 SELECT * FROM employees WHERE first_name = 'Irena'
 OR first_name = 'Vidya'
-OR first_name = 'Maya';
+OR first_name = 'Maya'
+AND gender = '%M';
+
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+
+
+
+
