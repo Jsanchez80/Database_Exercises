a3fd54f5ea233608db23ce4545bd13e8bca81ee8
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-20 11:25:56 -0500

    Update: Puhsing

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,3 +1,5 @@
 SELECT * FROM employees WHERE first_name IN('Irena','Vidya', 'Maya')
 ORDER BY first_name ASC;
 
+SELECT * FROM employees WHERE first_name IN('Irena','Vidya', 'Maya')
+ORDER BY first_name AND last_name ASC;
