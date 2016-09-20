40e9095cb28a74e8e1cdaaf185a84cce09b75deb
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-20 11:50:42 -0500

    Update: Solved for Question 5:
    Find the unique last names with a 'q' but not 'qu'. You may use either DISTINCT or GROUP BY. Your results should be:

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,6 +1,17 @@
+SELECT DISTINCT title FROM titles;
+
 SELECT DISTINCT title FROM titles
 ORDER BY title ASC;
 
 SELECT * FROM employees WHERE last_name LIKE 'E%'
 AND last_name LIKE '%E'
 GROUP BY last_name;
+
+SELECT DISTINCT * FROM employees WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
+GROUP BY first_name ASC;
+
+SELECT last_name FROM employees 
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+group BY last_name;
