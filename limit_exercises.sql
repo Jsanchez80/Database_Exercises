8cb7fe32f9d2a9a4f35c74440c50006d173ad9c9
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-20 12:06:39 -0500

    Update:
    Solved for Question 3.
    
    	Find your query for employees born on Christmas and hired in the 90s from order_by_exercises.sql. Update it to find just the first 5 employees. Their names should be:
    
    									 Khun Bernini
    									 Pohua Sudkamp
    									 Xiaopeng Uehara
    									 Irene Isaak
    									 Dulce Wrigley

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,3 +2,9 @@ SELECT DISTINCT last_name FROM employees
 WHERE last_name LIKE 'Z%'
 GROUP BY last_name DESC
 LIMIT 10;
+
+-- 3.
+SELECT * FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25'
+Order BY birth_date, hire_date DESC
+LIMIT 5;
