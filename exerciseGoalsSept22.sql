0eac646c6cd56d6d0bc916824c486fef28ef0c8d
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-22 12:08:27 -0500

    Update: Working on Solving Question 3. Outlined the thought process for this query and the solution to come.

--- exerciseGoalsSept22.sql
+++ exerciseGoalsSept22.sql
@@ -15,10 +15,10 @@ WHERE hire_date = '1990-10-22';
 
 -- Question 2. Find all the titles held by all employees with the first name Aamod.
 DESCRIBE titles;
--- Query produced Field Types as follows
+-- Query produced Field Types as follows:
 --  emp_no, title, from_date, to dated
 DESCRIBE employees;
--- Query produced Field Types as follows
+-- Query produced Field Types as follows:
 -- emp_no, birth_date, first_name, last_name, gender and hire_date
 
 -- Similar fields in both data sets are emp_no. You should be able to create a join on the emp_no.
@@ -27,3 +27,26 @@ DESCRIBE employees;
 SELECT *
 FROM employees
 WHERE first_name = 'Aamod';
+
+-- Query produced below allows you to see how the employee grew in their company possibly as their education improved or their title changed due to promotion/education. 
+-- Now that you have a viable solution, let's work on refactoring data to produce a subquery based on the solution below.
+   
+Select hire_date, first_name, last_name, title, birth_date
+FROM employees AS e
+JOIN titles as t
+	ON e.emp_no = t.emp_no
+WHERE e.first_name = 'Aamod'
+ORDER BY e.last_name;
+
+-- Question 3. Find all the department managers that are female.
+DESCRIBE employees;
+-- Query produced Field Types as follows:
+-- emp_no, birth_date, first_name, last_name, gender and hire_date
+DESCRIBE dept_manager;
+-- Query produced Field Types as follows:
+-- dept_no, emp_no, from_date and to_date
+DESCRIBE departments;
+-- Query produced Field Types as follows:
+-- dept_no, dept_name 
+
+
