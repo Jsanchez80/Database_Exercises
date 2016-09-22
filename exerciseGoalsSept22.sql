6c4d6b0c084253b93e5e846b35df215fe8572478
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-22 11:36:59 -0500

    Solving for Question 1: need to refactor to look like a subquery search.

--- exerciseGoalsSept22.sql
+++ exerciseGoalsSept22.sql
@@ -1,4 +1,11 @@
--- Find all employees with the same hire date as employee 101010 using a subquery:
+-- Question 1. Find all employees with the same hire date as employee 101010 using a subquery:
 
+-- Solution Note - found the date of employee 101010 by searching database using the following information: 
 SELECT first_name, last_name, emp_no, hire_date FROM employees
 WHERE emp_no = 101010;
+
+-- Soulution Note - Once I determined the hire date of employee 101010, I used the following query to list employees with the same hire dated of employee 101010. Now I need to refactor this using a subquery. 
+SELECT first_name, last_name, emp_no, hire_date 
+FROM employees
+WHERE hire_date = '1990-10-22';
+
