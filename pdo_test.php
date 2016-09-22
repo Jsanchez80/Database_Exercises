45a5dfd12d8367246e8d1abe467d2fb6184ac1bb
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-09-22 14:52:45 -0500

    Update: Solved for Question 3 in the exercise:
    		Create a second file called pdo_test.php. This file will:
    		Use require to include db_connect.php.
    		Echo the PDO connection status

---
+++ pdo_test.php
@@ -0,0 +1,10 @@
+<?php
+
+// Create a second file called pdo_test.php. This file will:
+// Use require to include db_connect.php.
+// Echo the PDO connection status
+
+require'db_connect.php';
+
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
